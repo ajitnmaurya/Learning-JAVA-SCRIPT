29e4538394b8b7192a426aace760e472b204037a
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-01-10 18:21:32 +0000

    variable, datatype

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js as newer version
+
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3); 
+
+console.log("Ajit");
+
+
+let name = "Ajit"
+let age = 21
+let isLoggedIn = false
+
+
+console.log(typeof "AJIT");
