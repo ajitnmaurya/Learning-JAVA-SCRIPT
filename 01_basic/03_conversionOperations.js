f2c6f6d5a37c015349fddc0266770df7e671e9a1
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-01-11 04:53:50 +0000

    conversion of data types

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = 33;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber )
+
+let isLoggedIn = "485"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
