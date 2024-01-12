c886dbaebb66350f657ffa63620c815b03679489
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-01-12 17:52:06 +0000

    converion of Operations

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,6 +1,6 @@
 let score = 33;
 
-// console.log(typeof score);
+8// console.log(typeof score);
 // console.log(typeof(score));
 
 let valueInNumber = Number(score)
@@ -10,4 +10,37 @@ let valueInNumber = Number(score)
 let isLoggedIn = "485"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//****************opertation********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Ajit"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2 );
+// console.log("1" + 2 + 2);
+// console.log(2 +2 + "2");
+
+// console.log((3+4)* 5 % 3 ) ;
+
+//console.log(+true);
+
+
+let gameCounter = 100
+
+++gameCounter;
+
+console.log(gameCounter);
