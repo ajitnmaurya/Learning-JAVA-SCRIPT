a34e8cb90087183b0a0ba53922444e190dbb8054
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-01-20 07:40:08 +0000

    math and num

--- 01_basic/06_nums_and_math.js
+++ 01_basic/06_nums_and_math.js
@@ -25,9 +25,17 @@ const hundreds = 1000000
 // console.log(Math.min(4,5,8,9,5));
 // console.log(Math.min(5,85,8,44,88));
 
-console.log(Math.random());
-console.log(Math.random()*10);
+//console.log(Math.random());
+// console.log(Math.random()*10);
 
-console.log(Math.random()*100);
+// console.log(Math.random()*100);
 
-console.log(Math.random()*1000);
+// console.log(Math.random()*1000);
+
+//console.log((Math.random()*10) + 1);
+
+const min =10
+const max = 20
+//
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min+1)) + min);
