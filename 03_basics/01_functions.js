08f25a5e6f371974a6eec91190bb7385bc123858
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-01-30 19:52:52 +0000

    functions_part_2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,4 +37,35 @@ function loginUserMessage (username = "sam"){
 }
 
 //console.log(loginUserMessage("Ajit"))
-console.log(loginUserMessage("Ajit"))
+//console.log(loginUserMessage("Ajit"));
+
+function calculateCartPrice(val1, val2, ...num1){
+   
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 300, 340));
+
+const user ={
+    username: "Ajit",
+    prices: 199
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+//handleObject(user)
+ //handleObject({
+ //   username: "Twinkle",
+//     price: 343 
+// })
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,600,100]));
