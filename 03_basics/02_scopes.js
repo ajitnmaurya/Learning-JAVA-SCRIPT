a4bc85aac9b97c1d39d495014d426ac62bf8dd28
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-02-01 16:02:24 +0000

    scope nested scope or etc

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,14 +4,55 @@ let a = 300
 if(true){
     let a = 10 
     const b = 20
-    console.log("Inner:", a);
+    //console.log("Inner:", a);
     function addnum(){
         
     }
 }
 
 
-
-console.log(a);
+//console.log(a);
 // console.log(b);
 //console.log(c);
+
+function one(){
+    const username = "Ajit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+//one()
+
+if(true){
+    const username = "Ajit"
+    if(username === "Ajit"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+// ++++++++++++++++++ intersting ++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
