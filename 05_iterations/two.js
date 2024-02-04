2a887db81a2692d7299d24565bc966baa290b570
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-02-04 20:49:54 +0000

    while ,    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,30 @@
+
+//let index = 0;
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);  
+//     index = index + 2
+// }
+
+//let myArray = ["flash", "batman", "superman", "wonderwomen"]
+//let index = 0
+// while (index < myArray.length){
+//     //console.log(`value is ${myArray[index]}`);
+//     index ++
+// }
+
+// let i = 1
+// let j = 1
+// while(i<10){
+//     console.log("table of this:" + i);
+//     while(j < 10){
+//         console.log(i + " * " + j + " = " + i*j)
+//         j++
+//     }
+//     i++
+// }
+
+let i = 0
+do {
+    console.log(i)
+    i++
+} while (i<10);
