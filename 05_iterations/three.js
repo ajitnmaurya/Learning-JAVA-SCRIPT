10e1a6573bdd4a50dc748c7c67a06249e4a0c4e8
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-02-05 13:21:18 +0000

    for of, map

---
+++ 05_iterations/three.js
@@ -0,0 +1,53 @@
+//for of
+
+//["", "", ""]
+//[{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//        console.log(num);
+// }
+
+// const greetings = "Hello world"
+// for (const greet of greetings) {
+//     console.log(`each character is ${greet}`);
+    
+// }
+
+// const greetings = "Hello world"
+// for (const greet of greetings) {
+//     if(greet == " "){
+
+//         console.log(`space detected`);
+//         continue
+//     }
+//     console.log(`each character is ${greet}`);
+    
+// }
+
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United State Of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+// Object for forof loop
+
+const myObject = {
+    game1 : 'NFS',
+    game2 : 'GTA'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
