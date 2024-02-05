20048dac6e3e6a77a4a14522f07541c25c2cebea
AJIT KUMAR SINGH <129522341+ajitnmaurya@users.noreply.github.com>
2024-02-05 13:21:47 +0000

    for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,38 @@
+const myObject = {
+    js : 'javaScript',
+    cpp : 'C++',
+    swift : "swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(key);
+//}
+// //To find a keys value
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} is shortcut for ${myObject[key]}`);
+// }
+
+
+//array with forin
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//         console.log(programming[key]);
+// }
+
+
+//forin loop in map ---- cannt possible in forin loop, map can't be iteration so we have to only use the forof
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United State Of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
